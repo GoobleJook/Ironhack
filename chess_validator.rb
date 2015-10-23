250c69500ce5031b6cd5048cfe42e674cb8d3489
GoobleJook <robert@smallbizninja.com>
2015-10-23 15:06:06 -0400

    Complete chess_validator.rb piece instatiations (both colors) and black piece tests

--- chess_validator.rb
+++ chess_validator.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-class Piece
+class Piece #master piece
 	def initialize (x, y, color)
 		@x = x
 		@y = y
@@ -9,7 +9,7 @@ class Piece
 end
 
 class Rook < Piece
-	def move (new_x, new_y)
+	def move (new_x, new_y) #valid moves 
 		if (@x - new_x).abs != 0 && (@y - new_y).abs == 0
 			true
 		elsif (@y - new_y).abs !=0 && (@x - new_x).abs == 0
@@ -21,7 +21,7 @@ class Rook < Piece
 end
 
 class Knight < Piece
-	def move (new_x, new_y)
+	def move (new_x, new_y) #valid moves 
 		if (@x - new_x).abs == 2 && (@y - new_y).abs == 1
 			true
 		elsif (@y - new_y).abs == 2 && (@x - new_x).abs == 1
@@ -32,8 +32,18 @@ class Knight < Piece
 	end
 end
 
+class Bishop < Piece
+	def move (new_x, new_y) #valid moves 
+		if (@y - new_y).abs == (@x - new_x).abs
+			true
+		else
+			false
+		end
+	end
+end
+
 class Queen < Piece
-	def move (new_x, new_y)
+	def move (new_x, new_y) #valid moves 
 		if (@x - new_x).abs != 0 && (@y - new_y).abs == 0
 			true
 		elsif (@y - new_y).abs !=0 && (@x - new_x).abs == 0
@@ -44,21 +54,126 @@ class Queen < Piece
 			false
 		end
 	end
-end #anti-knight won't work
+end
 
+class King < Piece
+	def move (new_x, new_y) #valid moves 
+		return false if (@x - new_x).abs > 1 || (@y - new_y).abs > 1
+		true 		
+	end
+end
+
+class Pawn < Piece
+	def move (new_x, new_y) #valid moves
+		return false if (
+			(@x - new_x).abs > 0 ||
+			(@y - new_y).abs > 2 ||
+			@color == "black" && @y - new_y > 0 ||
+			@color == "white" && @y - new_y < 0
+			)
+		true
+	end
+end
+
+# ===== populate the black team ====
 
 br1 = Rook.new(1, 8, "black")
 br2 = Rook.new(8, 8, "black")
+bk1 = Knight.new(2, 8, "black")
+bk2 = Knight.new(7, 8, "black")
+bb1 = Bishop.new(3, 8, "black")
+bb2 = Bishop.new(6, 8, "black")
+bq = Queen.new(4, 8, "black")
+bk = King.new(5, 8, "black")
+bp1 = Pawn.new(1, 7, "black")
+bp2 = Pawn.new(2, 7, "black")
+bp3 = Pawn.new(3, 7, "black")
+bp4 = Pawn.new(4, 7, "black")
+bp5 = Pawn.new(5, 7, "black")
+bp6 = Pawn.new(6, 7, "black")
+bp7 = Pawn.new(7, 7, "black")
+bp8 = Pawn.new(8, 7, "black")
+
+# ==== populate the white team ====
+
 wr1 = Rook.new(1, 1, "white")
 wr2 = Rook.new(8, 1, "white")
-wk1 = Knight.new(2,1, "white")
-wk2 = Knight.new(7,1, "white")
-wq = Queen.new(1,4, "white")
-bq = Queen.new(8,4, "black")
+wk1 = Knight.new(2, 1, "white")
+wk2 = Knight.new(7, 1, "white")
+wb1 = Bishop.new(1, 3, "white")
+wb2 = Bishop.new(1, 6, "white")
+wq = Queen.new(1, 4, "white")
+wk = King.new(1, 5, "white")
+wp1 = Pawn.new(1, 2, "white")
+wp2 = Pawn.new(2, 2, "white")
+wp3 = Pawn.new(3, 2, "white")
+wp4 = Pawn.new(4, 2, "white")
+wp5 = Pawn.new(5, 2, "white")
+wp6 = Pawn.new(6, 2, "white")
+wp7 = Pawn.new(7, 2, "white")
+wp8 = Pawn.new(8, 2, "white")
 
-puts br1.move(8,2)
-puts br1.move(1,3)
-puts wk2.move(6,3)
-puts wk2.move(6,2)
-puts bq.move(8,5)
-puts bq.move(8,)
+# ======= black team tests =====
+puts "BLACK TEAM TESTS FOLLOW:"
+
+if  #rooks
+	br1.move(1, 1) == true &&
+	br2.move(8, 6) == true &&
+	br1.move(2, 7) == false &&
+	br2.move(7, 6) == false  
+	then puts "Black rooks: PASS" 
+else
+	puts "Black rooks: FAIL"
+	puts "Exiting tests"
+	abort
+end
+
+if  #knights
+	bk1.move(3, 6) == true &&
+	bk2.move(8, 6) == true &&
+	bk1.move(3, 7) == false &&
+	bk2.move(7, 6) == false  
+	then puts "Black knights: PASS" 
+else
+	puts "Black knights: FAIL"
+	puts "Exiting tests"
+	abort
+end
+
+if  #bishops
+	bb1.move(6, 5) == true &&
+	bb2.move(1, 3) == true &&
+	bb1.move(7, 8) == false &&
+	bb2.move(6, 7) == false  
+	then puts "Black bishops: PASS" 
+else
+	puts "Black bishops: FAIL"
+	puts "Exiting tests"
+	abort
+end
+
+if  #queen
+	bq.move(5, 7) == true &&
+	bq.move(4, 2) == true &&
+	bq.move(3, 6) == false &&
+	bq.move(5, 2) == false  
+	then puts "Black queen: PASS" 
+else
+	puts "Black queen: FAIL"
+	puts "Exiting tests"
+	abort
+end
+
+if  #king
+	bk.move(6, 8) == true &&
+	bk.move(5, 7) == true &&
+	bk.move(5, 6) == false &&
+	bk.move(3, 7) == false  
+	then puts "Black king: PASS"
+	puts "\n\nALL TESTS PASS!!!" 
+else
+	puts "Black king: FAIL"
+	puts "Exiting tests"
+	abort
+end
+
