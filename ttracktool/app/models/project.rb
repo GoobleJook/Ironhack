29ec402497ef6b1440b5f274390e4de02c25d51e
GoobleJook <robert@smallbizninja.com>
2015-11-25 06:28:17 -0500

    Add logic to model

--- ttracktool/app/models/project.rb
+++ ttracktool/app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
 	def self.iron_find(id)
 		where(id: id).first
 	end
+
+	def self.last_created_projects(limit)
+		@projects = Project.order(created_at: :desc).limit(10)
+	end
 end
