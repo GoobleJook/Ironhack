711125578abacb022141229aeba3fbf3cf116b83
GoobleJook <robert@smallbizninja.com>
2015-11-24 21:00:40 -0500

    Add clean method, correct file

--- our_first_project/app/models/project.rb
+++ our_first_project/app/models/project.rb
@@ -1,10 +1,3 @@
 class Project < ActiveRecord::Base
-	def self.iron_find(id)
-		where(id: id).first
-	end
-
-	def ::clean_old
-		p = Project.where("created_at < ?", 1.week.ago.utc)
-		p.destroy
-	end	
+	
 end
