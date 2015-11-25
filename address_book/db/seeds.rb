3fe6bad0993650e883dc775399d2cc1d799ad84a
GoobleJook <robert@smallbizninja.com>
2015-11-25 06:44:02 -0500

    Add address_book model and controller

--- address_book/db/seeds.rb
+++ address_book/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Contact.create(name: 'Robert', address: 'Brickell', phone: '555-555-5555', email: 'rob@rob.rob')
+
+Contact.create(name: 'Tim', address: 'Brickells', phone: '555-555-4555', email: 'tim@rob.rob')
+
+Contact.create(name: 'Kim', address: 'Brickellsss', phone: '555-545-5555', email: 'kim@rob.rob')
+
+Contact.create(name: 'Jim', address: 'Brickellssss', phone: '535-555-5555', email: 'seed@rob.rob')
